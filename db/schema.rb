a404574a4665f676838940bef0e195fcea6f585c
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-03-10 14:44:18 +0100

    index item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_10_093940) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_10_131725) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_10_093940) do
     t.string "title"
     t.text "description"
     t.decimal "price"
-    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
