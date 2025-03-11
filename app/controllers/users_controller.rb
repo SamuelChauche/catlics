5140481f4a85dceba9040b039f325dd63260e6df
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-03-11 20:34:19 +0100

    rajout des photos des enciennes commende

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,10 @@ class UsersController < ApplicationController
   def show
     # @orders = current_user.orders
     @user = current_user
+    # @past_order_images = @user.orders.order_items.item.image
+    @images = @user.orders.includes(order_items: :item).map { |order| order.order_items.map { |order_item| order_item.item.image } }.flatten
+
+    @orders = @user.orders
+
   end
 end
