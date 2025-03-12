bb6fe3b4cfc812d060f967fb8735b2a82dd4b6c5
Wieedze <maxime.moodz@gmail.com>
2025-03-12 09:12:47 +0100

    user controler final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @item = Item.find(params[:id])
-    @cart = current_user.cart
-    @cart_items = @cart.cart_items.includes(:item)
-    @orders = current_user.orders.includes(cart_items: :item)
     @user = current_user
     # @past_order_images = @user.orders.order_items.item.image
     @images = @user.orders.includes(order_items: :item).map { |order| order.order_items.map { |order_item| order_item.item.image } }.flatten
 
     @orders = @user.orders
-
   end
 end
