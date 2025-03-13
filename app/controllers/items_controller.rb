de3273740bcfd374422aa76dc2b4c9bf13bd4989
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-03-13 15:27:31 +0100

    modif over menu deroulan

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,18 @@
 class ItemsController < ApplicationController
   def index
     breeds = params[:options]
+    p "#"*111
     if params[:options] == nil || breeds.length == 0
-      items = Item.order("RANDOM()")
+      items = Item
     else
-      items = Item.joins(:breed).where(breeds: { name: breeds }).order("RANDOM()")
+      items = Item.joins(:breed).where(breeds: { name: breeds })
+    end
+    if params[:sort] == "price_asc"
+      items = items.order(price: :asc)
+    elsif params[:sort] == "price_desc"
+      items = items.order(price: :desc)
+    elsif params[:sort] == "random"
+      items = items.order("RANDOM()")
     end
     @items= items.paginate(page: params[:page], per_page: 11)
   end
